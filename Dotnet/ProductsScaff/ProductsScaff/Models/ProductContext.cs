c28c8fe978845cb9a881f5d6a6ac4bebacfad3a2
Umamaheswar Reddy <umamaheswarreddy2113@gmail.com>
2026-02-16 10:10:44 +0530

    ProductsScaff

---
+++ Dotnet/ProductsScaff/ProductsScaff/Models/ProductContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductsScaff.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+                : base(options)
+        { }
+        public DbSet<ProductsItem> ProductsItems { get; set; } = null!;
+    }
+}
---
+++ Dotnet/ProductsScaff/ProductsScaff/Models/ProductsItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductsScaff.Models
+{
+    public class ProductsItem
+    {
+        public long Id { get; set; }          // Primary Key
+        public string? Name { get; set; }     // Product name
+        public decimal Price { get; set; }    // Product price
+        public int Quantity { get; set; }     // Stock quantity
+        public bool IsAvailable { get; set; } // Availability status
+    }
+}
